14dca7e2643a86eea0e1baba73336ad170aebc49
theo_faedo <theo.faedo@gmail.com>
2023-07-17 23:58:34 +0200

    adding consumptionatday request tests

--- NutrifitAPI/src/Controllers/ConsumptionAtDateController.php
+++ NutrifitAPI/src/Controllers/ConsumptionAtDateController.php
@@ -15,6 +15,10 @@ require __DIR__ . '/../../vendor/autoload.php';
 
 class ConsumptionAtDateController extends Controller{
 
+    public function __construct(\DI\Container $container){
+        parent::__construct($container);    
+    }
+
     /**
      * Authenticate the user
      * @return Result json with sucess value, json with error message if error
@@ -24,18 +28,24 @@ class ConsumptionAtDateController extends Controller{
 
         $params = $rq->getQueryParams();
 
-        $authhelper = new AuthHelper();
+        $authhelper = new AuthHelper($this->container->get('session'), $this->container->get('staticexecutor'));
 
         if($authhelper->authentified()){
             $idUser = $authhelper->getIdUserAuthentified();
 
             $date = date('Y-m-d');
-            $consumptionAtDay = Consumption::whereDate('consumed_on', '=', $params['date'])->where('idUser', $idUser)->get();
+            if(isset($params['date'])){
+                $consumptionAtDay = Consumption::whereDate('consumed_on', '=', $params['date'])->where('idUser', $idUser)->get();
             
-            foreach($consumptionAtDay as $consumption){
-                $res[$consumption->idConsumption] = $consumption;
+                foreach($consumptionAtDay as $consumption){
+                    $res[$consumption->idConsumption] = $consumption;
+                }
+            }else{
+                $res['error'] = "Missing parameters";
+                $rs= $rs->withStatus(400);
+                $rs->getBody()->write(json_encode($res));
+                return $rs->withHeader('Content-Type', 'application/json');
             }
-
             $rs= $rs->withStatus(200);
         }else{
             $res['error'] = "Not authentified";
---
+++ NutrifitAPI/tests/Requests/ConsumptionAtDateRequestTest.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace Tests\Requests;
+
+use Tests\TestCase;
+use App\Helpers\AuthHelper;
+
+use App\Helpers\DBConnection;
+
+use App\Application\Session;
+use App\Application\StaticExecutor;
+
+use App\Models\Consumption;
+use App\Models\Consumable;
+use App\Models\User;
+
+class ConsumptionAtDateRequestTest extends TestCase{
+
+    protected function setUp(): void{
+        parent::setUp();
+    }
+
+    protected function tearDown(): void{
+        parent::tearDown();
+        Consumption::query()->delete();
+        Consumable::query()->delete();
+        User::query()->delete();
+    }
+
+    public function testSuccess(){
+        $app = $this->getAppInstance();
+        $container = $app->getContainer();
+
+        $this->createDataSet();
+
+        $sessionMock = $container->get('session');
+        $sessionMock = $this->authentifyByMock($sessionMock);
+        $sessionMock->expects($this->any())
+            ->method('getItem')
+            ->with('user')
+            ->willReturn(['idUser' => 99999]);
+
+        $request = $this->createRequest('GET', '/consumptionatdate/')
+        ->withQueryParams(['date' => '2019-01-01']);
+        $response = $app->handle($request);
+
+        $json = (string) $response->getBody();
+        $data = json_decode($json, true);
+
+        $this->assertEquals([1 => [
+            'idConsumption' => 1,
+            'idConsumable' => 1,
+            'idUser' => 99999,
+            'consumed_on' => '2019-01-01 00:00:00',
+            'last_update' => '2019-01-01 00:00:00',
+            'proportion' => 1
+        ]], $data);
+
+        $request = $this->createRequest('GET', '/consumptionatdate/')
+        ->withQueryParams(['date' => '2019-01-02']);
+        $response = $app->handle($request);
+
+        $json = (string) $response->getBody();
+        $data = json_decode($json, true);
+
+        $this->assertEquals([], $data);
+    }
+
+    public function testMissingParameters(){
+        $app = $this->getAppInstance();
+        $container = $app->getContainer();
+
+        $sessionMock = $container->get('session');
+        $sessionMock = $this->authentifyByMock($sessionMock);
+        $sessionMock->expects($this->any())
+            ->method('getItem')
+            ->with('user')
+            ->willReturn(['idUser' => 99999]);
+
+        $request = $this->createRequest('GET', '/consumptionatdate/')
+        ->withQueryParams([]);
+        $response = $app->handle($request);
+
+        $json = (string) $response->getBody();
+        $data = json_decode($json, true);
+
+        $this->assertEquals(['error' => 'Missing parameters'], $data);
+    }
+
+    public function testNotAuthentified(){
+        $app = $this->getAppInstance();
+
+        $this->createDataSet();
+
+        $request = $this->createRequest('GET', '/consumptionatdate/')
+        ->withQueryParams([]);
+        $response = $app->handle($request);
+
+        $json = (string) $response->getBody();
+        $data = json_decode($json, true);
+
+        $this->assertEquals(['error' => "Not authentified"], $data);
+    }
+
+    function createDataSet(){
+        $user = new User();
+        $user->idUser = 99999;
+        $user->pseudo = 'test';
+        $user->mail = 'test@gmail.com';
+        $user->energy_goal = 2000;
+        $user->fats_goal = 70;
+        $user->carbohydrates_goal = 210;
+        $user->proteins_goal = 140;
+        $user->save();
+        
+        $consumable = new Consumable();
+        $consumable->idConsumable = 1;
+        $consumable->name = 'test1';
+        $consumable->author = 99999;
+        $consumable->public = 1;
+        $consumable->energy = 100;
+        $consumable->proteins = 100;
+        $consumable->fats = 100;
+        $consumable->carbohydrates = 100;
+        $consumable->quantity_label = '100g';
+        $consumable->type = 'MEAL';
+        $consumable->save();
+
+        $recipecomposition = new Consumption();
+        $recipecomposition->idConsumption = 1;
+        $recipecomposition->idConsumable = 1;
+        $recipecomposition->idUser = 99999;
+        $recipecomposition->consumed_on = '2019-01-01';
+        $recipecomposition->last_update = '2019-01-01';
+        $recipecomposition->proportion = 1;
+        $recipecomposition->save();
+
+    }
+
+} 
