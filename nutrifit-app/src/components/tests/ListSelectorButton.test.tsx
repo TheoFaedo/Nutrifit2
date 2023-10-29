5c875a980f8a6b2ce7a7f55f9ddf804206f81a73
theo_faedo <theo.faedo@gmail.com>
2023-10-29 11:40:07 +0100

    tests for ListSelectorButton.tsx rendering and interaction

---
+++ nutrifit-app/src/components/tests/ListSelectorButton.test.tsx
@@ -0,0 +1,48 @@
+import { render, screen } from "@testing-library/react";
+import ListSelectorButton from "../ListSelectorButton";
+
+
+describe("ListSelectorButton component rendered correctly", () => {
+    test("buttons rendered correctly with active 0", () => {
+        render(<ListSelectorButton names={["categ1", "categ2"]} active={0}/>);
+        const button1Elmnt = screen.getByRole("button", {name: "categ1"});
+        const button2Elmnt = screen.getByRole("button", {name: "categ2"});
+        expect(button1Elmnt).toBeInTheDocument();
+        expect(button2Elmnt).toBeInTheDocument();
+
+        expect(button1Elmnt).toHaveClass("border-b-4");
+        expect(button2Elmnt).not.toHaveClass("border-b-4");
+    })
+
+    test("buttons rendered correctly with active 1", () => {
+        render(<ListSelectorButton names={["categ1", "categ2"]} active={1}/>);
+        const button1Elmnt = screen.getByRole("button", {name: "categ1"});
+        const button2Elmnt = screen.getByRole("button", {name: "categ2"});
+        expect(button1Elmnt).toBeInTheDocument();
+        expect(button2Elmnt).toBeInTheDocument();
+
+        expect(button1Elmnt).not.toHaveClass("border-b-4");
+        expect(button2Elmnt).toHaveClass("border-b-4");
+    })
+}) 
+
+describe("ListSelectorButton component interact correctly", () => {
+
+    test("onClick handler is called on a button click", () => {
+        const handler = jest.fn();
+
+        render(<ListSelectorButton names={["categ1", "categ2"]} active={0} onClick={handler}/>);
+        
+        const button1Elmnt = screen.getByRole("button", {name: "categ1"});
+        const button2Elmnt = screen.getByRole("button", {name: "categ2"});
+        
+        button1Elmnt.click();
+        expect(handler).toHaveBeenCalledTimes(1);
+
+        button2Elmnt.click();
+        expect(handler).toHaveBeenCalledTimes(2);
+
+        button2Elmnt.click();
+        expect(handler).toHaveBeenCalledTimes(3)
+    })
+});
