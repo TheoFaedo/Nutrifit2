bc6bd9977732a9834a0e16e4efdace2170c246c9
theo_faedo <theo.faedo@gmail.com>
2023-10-29 14:50:20 +0100

    tests for ListSelectorButton with new comportements

--- nutrifit-app/src/components/tests/ListSelectorButton.test.tsx
+++ nutrifit-app/src/components/tests/ListSelectorButton.test.tsx
@@ -24,6 +24,35 @@ describe("ListSelectorButton component rendered correctly", () => {
         expect(button1Elmnt).not.toHaveClass("border-b-4");
         expect(button2Elmnt).toHaveClass("border-b-4");
     })
+
+    test("buttons rendered correctly with multiple names", () => {
+        render(<ListSelectorButton names={["categ1", "categ2", "categ3", "categ4"]} active={0}/>);
+        const button1Elmnt = screen.getByRole("button", {name: "categ1"});
+        const button2Elmnt = screen.getByRole("button", {name: "categ2"});
+        const button3Elmnt = screen.getByRole("button", {name: "categ3"});
+        const button4Elmnt = screen.getByRole("button", {name: "categ4"});
+        expect(button1Elmnt).toBeInTheDocument();
+        expect(button2Elmnt).toBeInTheDocument();
+        expect(button3Elmnt).toBeInTheDocument();
+        expect(button4Elmnt).toBeInTheDocument();
+
+        expect(button1Elmnt).toHaveClass("border-b-4");
+        expect(button2Elmnt).not.toHaveClass("border-b-4");
+        expect(button3Elmnt).not.toHaveClass("border-b-4");
+        expect(button4Elmnt).not.toHaveClass("border-b-4");
+    })
+
+    test("buttons rendered correctly with active number too high", () => {
+        render(<ListSelectorButton names={["categ1", "categ2"]} active={2}/>);
+        const button1Elmnt = screen.getByRole("button", {name: "categ1"});
+        const button2Elmnt = screen.getByRole("button", {name: "categ2"});
+
+        expect(button1Elmnt).toBeInTheDocument();
+        expect(button2Elmnt).toBeInTheDocument();
+
+        expect(button1Elmnt).toHaveClass("border-b-4");
+        expect(button2Elmnt).not.toHaveClass("border-b-4");
+    })
 }) 
 
 describe("ListSelectorButton component interact correctly", () => {
